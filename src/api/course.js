d685421a670eb4ccb9def34e878e2ede23f23b62
francoterragna <francoterragna@hotmail.com>
2023-08-07 22:25:16 -0300

    paginacion de cursos terminada

--- src/api/course.js
+++ src/api/course.js
@@ -9,10 +9,8 @@ export class Course {
             const limitFilter = `limit=${params?.limit || 10}`;
 
             const url = `${this.baseApi}/${ENV.API_ROUTES.COURSES}?${pageFilter}&${limitFilter}`;
-            console.log(url);
 
             const response = await fetch(url);
-            console.log(response);
             const result = await response.json();
 
             if(response.status !==200) throw result;
--- src/components/Admin/Course/ListCourses/ListCourses.jsx
+++ src/components/Admin/Course/ListCourses/ListCourses.jsx
@@ -1,26 +1,58 @@
 import React, { useState, useEffect } from 'react';
-import { Course } from '../../../../api'
+import { Loader, Pagination } from 'semantic-ui-react';
+import { size, map } from 'lodash';
+import { CourseItem } from '../CourseItem';
+import { Course } from '../../../../api';
+import './ListCourses.scss'
 
 const courseController = new Course();
 
 export function ListCourses() {
   const [courses, setCourses] = useState(false);
+  const [page, setPage] = useState(1);
+  const [pagination, setPagination] = useState();
 
   useEffect(() => {
     (async () => {
       try {
-        const response = await courseController.getCourses();
+        const response = await courseController.getCourses({page, limit:5});
         console.log(response);
+        setCourses(response.docs);
+        setPagination({
+          limit: response.limit,
+          page: response.page,
+          pages: response.totalPages,
+          total: response.totalDocs
+        })
       } catch (error) {
-        console.error('hola');
+        console.error(error);
       }
     })()
-  }, [])
+  }, [page]);
+
+  const changePage = (_, data) => {
+    setPage(data.activePage)
+  }
   
+  if(!courses) return <Loader active inline='centered' />
+  if(size(courses) === 0) return 'No hay ningún curso';;
 
   return (
-    <div>
-        <h2>Lista de cursos</h2>
+    <div className='list-courses'>
+        {map(courses, (course) => (
+          <CourseItem key={course._id} course={course} />
+        ))}
+
+        <div className='list-courses__pagination'>
+          <Pagination
+            totalPages={pagination.pages}
+            defaultActivePage={pagination.page}
+            ellipsisItem={null}
+            firstItem={null}
+            lastItem={null}
+            onPageChange={changePage}
+          />
+        </div>
     </div>
   )
 }
